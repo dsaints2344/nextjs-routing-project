a7c034628a1568bb458129f6646361610b71c610
David De Los Santos <30784827+dsaints2344@users.noreply.github.com>
2023-04-07 20:53:17 -0400

    handling event details conditional rendering

--- pages/events/[eventId].tsx
+++ pages/events/[eventId].tsx
@@ -1,21 +1,34 @@
-import { useRouter } from 'next/router';
-import { FC, useEffect } from 'react';
-import { getEventById } from '../api/dummy-data';
+import { useRouter } from "next/router";
+import { FC, Fragment, useEffect } from "react";
+import { getEventById } from "../api/dummy-data";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventDetailsPage: FC = () => {
   const router = useRouter();
 
   const eventId = Array.isArray(router.query.eventId)
-    ? router.query.eventId.join('')
+    ? router.query.eventId.join("")
     : router.query.eventId;
-  if (eventId) {
-    const event = getEventById(eventId);
-    if (event) {
-      return <p>Event not found</p>;
-    }
-  }
+  const event = getEventById(eventId!);
 
-  return <div>Event details page</div>;
+  return event ? (
+    <Fragment>
+      <EventSummary title={event!.title} />
+      <EventLogistics
+        date={event!.date}
+        address={event!.location}
+        image={event!.image}
+        imageAlt={event!.title}
+      />
+      <EventContent>
+        <p>{event!.description}</p>
+      </EventContent>
+    </Fragment>
+  ) : (
+    <p>Event not found</p>
+  );
 };
 
 export default EventDetailsPage;
